a96e02fac5b54e2e8e23a0505da97040349f4fbd
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-03-06 10:57:44 -0700

    build

---
+++ Event Bus/Assets/EventPublisher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPublisher : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            EventBus.TriggerEvent("Shoot");
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            EventBus.TriggerEvent("Launch");
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            EventBus.TriggerEvent("makeSentient");
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            EventBus.TriggerEvent("Game");
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            EventBus.TriggerEvent("Hack");
+        }
+    }
+}
